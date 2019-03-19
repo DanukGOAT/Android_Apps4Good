64fe832304863023c378fbda30e4a5c0b9e7e945
DanukGOAT <44783244+DanukGOAT@users.noreply.github.com>
2019-03-19 13:12:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/exampleapp/AnswerActivity.java
@@ -0,0 +1,25 @@
+package com.example.exampleapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+
+        //get the intent that started this activity and extract the data
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        Double ans = bundle.getDouble("answer");
+
+
+        TextView answerView = (TextView) findViewById(R.id.answerView);
+        answerView.setText(ans.toString());
+    }
+}
