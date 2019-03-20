8e1c2359355cf2922f0112770e7d66c3d36e6d2f
Nathan <ned883@gmail.com>
2019-03-20 13:37:09 -0400

    If you this can seee it be workin

---
+++ app/src/main/java/com/example/exampleapp/InitPage.java
@@ -0,0 +1,13 @@
+package com.example.exampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_page);
+    }
+}
