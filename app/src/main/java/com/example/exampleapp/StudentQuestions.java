634623c0fd6e916d3c1e276028e8bbb388fc1fba
DanukGOAT <44783244+DanukGOAT@users.noreply.github.com>
2019-03-25 09:42:53 -0400

    Ello Mates

--- app/src/main/java/com/example/exampleapp/StudentQuestions.java
+++ app/src/main/java/com/example/exampleapp/StudentQuestions.java
@@ -2,6 +2,7 @@ package com.example.exampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class StudentQuestions extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class StudentQuestions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_questions);
+
     }
 }
