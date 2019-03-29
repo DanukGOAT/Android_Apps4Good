2cdae7d68bc0c6fdad86874808f3d92dee0afe28
Nathan <ned883@gmail.com>
2019-03-29 14:28:55 -0400

    Debug button for main activity. Working on navbar

---
+++ app/src/main/java/com/example/exampleapp/NavBar.java
@@ -0,0 +1,17 @@
+package com.example.exampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class NavBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_bar);
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+    }
+}
