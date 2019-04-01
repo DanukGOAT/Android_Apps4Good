653c6be1a9816bcfdda5f6eafb9413d3ec4aa6f6
DanukGOAT <44783244+DanukGOAT@users.noreply.github.com>
2019-04-01 09:16:16 -0400

    Ello Mates

--- app/src/main/java/com/example/exampleapp/CalendarDisplay.java
+++ app/src/main/java/com/example/exampleapp/CalendarDisplay.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
@@ -26,7 +28,14 @@ public class CalendarDisplay extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        Button testButton = findViewById(R.id.testButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalendarDisplay.this, ProfileInfo.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
