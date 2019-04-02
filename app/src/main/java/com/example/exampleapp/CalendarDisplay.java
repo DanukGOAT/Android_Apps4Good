5dcd848ee08da038f1c8762f6bbb72a477650fe3
DanukGOAT <44783244+DanukGOAT@users.noreply.github.com>
2019-04-02 12:20:25 -0400

    Ello Mates

--- app/src/main/java/com/example/exampleapp/CalendarDisplay.java
+++ app/src/main/java/com/example/exampleapp/CalendarDisplay.java
@@ -1,21 +1,34 @@
 package com.example.exampleapp;
 
 import android.content.Intent;
+import android.app.TimePickerDialog;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class CalendarDisplay extends AppCompatActivity {
+public class CalendarDisplay extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
     public String date="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_display);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+
 
         CalendarView calendarView = findViewById(R.id.Calendarr);
 
@@ -38,5 +51,11 @@ public class CalendarDisplay extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hour: " + hourOfDay + " Minute: " + minute);
     }
 }
