3148298895961bf680ec8164a2c2efc69db54042
Nathan <ned883@gmail.com>
2019-05-10 13:39:01 -0400

    Implemented RecyclerView

--- app/src/main/java/com/example/exampleapp/tutorSelection.java
+++ app/src/main/java/com/example/exampleapp/tutorSelection.java
@@ -1,6 +1,5 @@
 package com.example.exampleapp;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -15,11 +14,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-
-import android.view.ContextThemeWrapper;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
